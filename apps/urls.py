daa8672e6547b2aea53718bcfbb7c3d55e9a3e6b
bakhtiyarov-abdulloh <abdullohbaxtiyorov2004@gmail.com>
2024-08-16 18:56:26 +0500

    Abdulloh o company qoshildi

--- apps/urls.py
+++ apps/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import ProductList, CategoryView
+from .views import ProductList, CategoryView, OKompanyView, CompanyContactsView, ManufactureView
 
 urlpatterns = [
     path('', ProductList.as_view(), name='home_page'),
-    path('market/<slug:foo>/', CategoryView.as_view(), name='category_page'),
+    path('market/<slug:slug>/', CategoryView.as_view(), name='category_page'),
+    path('o-kompani/', OKompanyView.as_view(), name='o-kompany'),
+    path('kontakty', CompanyContactsView.as_view(), name='kontakt'),
+    path('proizvoditeli/', ManufactureView.as_view(), name='proizvaditeli'),
+
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, TemplateView
 
 from apps.models import Product, Category
 
@@ -10,18 +10,50 @@ class ProductList(ListView):
     paginate_by = 5
     context_object_name = "products"
 
+class OKompanyView(TemplateView):
+    template_name = 'from_company/from_company.html'
 
+class CompanyContactsView(TemplateView):
+    template_name = 'from_company/contact.html'
+
+class ManufactureView(TemplateView):
+    template_name = 'from_company/manufacturers.html'
 
 class CategoryView(ListView):
     template_name = 'apps/category_product_list.html'
-    model = Category
+    model = Product
     context_object_name = 'products'
 
     def get_queryset(self):
-        self.category = get_object_or_404(Category, slug=self.kwargs['foo'])
+        # Retrieve the category based on the slug passed in the URL
+        self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        # Filter products by this category
         return Product.objects.filter(category=self.category)
 
     def get_context_data(self, **kwargs):
+        # Get the base context from ListView
         context = super().get_context_data(**kwargs)
+        # Add the category to the context
         context['category'] = self.category
+        # Add the category's children to the context
+        context['children'] = self.category.get_children()
         return context
+
+
+
+# from django.shortcuts import render, get_object_or_404
+# from .models import Category
+#
+#
+# def category_detail(request, slug):
+#     # Retrieve the category based on the slug
+#     category = get_object_or_404(Category, slug=slug)
+#     # Get all children of the category
+#     children = category.get_children()
+#
+#     # Pass the category and its children to the template
+#     context = {
+#         'category': category,
+#         'children': children,
+#     }
+#     return render(request, 'category_detail.html', context)
